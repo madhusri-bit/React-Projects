5d84fa2c7f2e94223626ab02fe022b51065f8c10
madhusri-bit <150882578+madhusri-bit@users.noreply.github.com>
2024-08-08 12:08:12 +0530

    Update DAY1_CY2.jsx

--- DAY 1/DAY1_CY2.jsx
+++ DAY 1/DAY1_CY2.jsx
@@ -1,35 +1,13 @@
-import React, { useState } from 'react';
+function array(){
+    let arr1=["North","West","East","South"];
+     alert(arr1);
+}
 
-const ArrayManipulation = () => {
-  const [array, setArray] = useState(['North', 'South', 'East', 'West']);
-
-  const handleArrayButtonClick = () => {
-    const newArray = array.map(element => '$ {element} News');
-    setArray(newArray);
-  };
-
-  const handleRefreshArrayButtonClick = () => {
-    const joinedString = array.join(', ');
-    setArray([joinedString]);
-  };
-
-  return (
-    <div>
-      <h1>Array Manipulation</h1>
-      <div>
-        <button onClick={handleArrayButtonClick}>Array</button>
-        <button onClick={handleRefreshArrayButtonClick}>Refresh Array</button>
-      </div>
-      <div>
-        <h2>Array Elements:</h2>
-        <ul>
-          {array.map((element, index) => (
-            <li key={index}>{element}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-export default ArrayManipulation;
+function App(){
+    return(
+        <div className="App">
+            <button onClick={array}>array</button> 
+        </div>
+    )
+}
+export default App;
