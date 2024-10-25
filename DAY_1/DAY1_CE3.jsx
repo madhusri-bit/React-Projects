eebecfd6bb2d3ab4d431c484d80fb93c4e026570
[Madhusri T] <[madhusri0213@gmail.com]>
2024-10-25 22:38:26 +0530

    Renamed folder

---
+++ DAY_1/DAY1_CE3.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const World = () =>{
+    return(
+        <div>
+            <p>This is World Functional Component</p>
+        </div>
+    );
+};
+export default World;
